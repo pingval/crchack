8bbdd654dd5934b79e1ca72c47e8a151feada8fc
pingval <40169989+pingval@users.noreply.github.com>
2019-07-16 00:52:51 +0900

    add target option

--- crchack.h
+++ crchack.h
@@ -7,4 +7,10 @@ typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t u8;
 
+#define DEBUG 0
+// #define TARGETS_SIZE (0x2000 * 16) // PlayStation MemoryCard size
+#define TARGETS_SIZE (1024 * 1024 * 10) // 10 MB
+
+extern u8 TARGETS[TARGETS_SIZE];
+
 #endif
